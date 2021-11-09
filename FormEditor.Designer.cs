57377c9155c057c491453eb4d67f77a1e687e697
Marian Anton-Onofrei <marian.anton-onofrei@student.usv.ro>
2021-11-09 07:16:55 +0200

    Add project files.

---
+++ FormEditor.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace VideoEditor
+{
+    partial class FormEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormEditor));
+            this.pnVideoEditing = new System.Windows.Forms.Panel();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.pbCursor = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pnVideoEditing.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCursor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnVideoEditing
+            // 
+            this.pnVideoEditing.AllowDrop = true;
+            this.pnVideoEditing.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnVideoEditing.AutoScroll = true;
+            this.pnVideoEditing.AutoScrollMargin = new System.Drawing.Size(1200, 110);
+            this.pnVideoEditing.AutoScrollMinSize = new System.Drawing.Size(10, 10);
+            this.pnVideoEditing.Controls.Add(this.pbCursor);
+            this.pnVideoEditing.Location = new System.Drawing.Point(1, 393);
+            this.pnVideoEditing.Name = "pnVideoEditing";
+            this.pnVideoEditing.Size = new System.Drawing.Size(1429, 168);
+            this.pnVideoEditing.TabIndex = 0;
+            this.pnVideoEditing.Click += new System.EventHandler(this.pnVideoEditing_Click);
+            this.pnVideoEditing.DragDrop += new System.Windows.Forms.DragEventHandler(this.pnVideoEditing_DragDrop);
+            this.pnVideoEditing.DragEnter += new System.Windows.Forms.DragEventHandler(this.pnVideoEditing_DragEnter);
+            this.pnVideoEditing.DragLeave += new System.EventHandler(this.pnVideoEditing_DragLeave);
+            this.pnVideoEditing.Paint += new System.Windows.Forms.PaintEventHandler(this.pnVideoEditing_Paint);
+            this.pnVideoEditing.Enter += new System.EventHandler(this.pnVideoEditing_Enter);
+            this.pnVideoEditing.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnVideoEditing_MouseDown);
+            this.pnVideoEditing.MouseEnter += new System.EventHandler(this.pnVideoEditing_MouseEnter);
+            this.pnVideoEditing.MouseHover += new System.EventHandler(this.pnVideoEditing_MouseHover);
+            this.pnVideoEditing.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnVideoEditing_MouseMove);
+            this.pnVideoEditing.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnVideoEditing_MouseUp);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(722, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(594, 351);
+            this.axWindowsMediaPlayer1.TabIndex = 2;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // pbCursor
+            // 
+            this.pbCursor.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbCursor.Cursor = System.Windows.Forms.Cursors.VSplit;
+            this.pbCursor.Location = new System.Drawing.Point(146, 24);
+            this.pbCursor.Name = "pbCursor";
+            this.pbCursor.Size = new System.Drawing.Size(5, 50);
+            this.pbCursor.TabIndex = 0;
+            this.pbCursor.TabStop = false;
+            this.pbCursor.Click += new System.EventHandler(this.pbCursor_Click);
+            this.pbCursor.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbCursor_MouseDown);
+            this.pbCursor.MouseHover += new System.EventHandler(this.pbCursor_MouseHover);
+            this.pbCursor.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbCursor_MouseMove);
+            this.pbCursor.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbCursor_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FormEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1328, 520);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.pnVideoEditing);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Name = "FormEditor";
+            this.Text = "FormEditor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ClientSizeChanged += new System.EventHandler(this.FormEditor_ClientSizeChanged);
+            this.SizeChanged += new System.EventHandler(this.FormEditor_SizeChanged);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormEditor_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.FormEditor_KeyPress);
+            this.pnVideoEditing.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCursor)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnVideoEditing;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.PictureBox pbCursor;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ FormEditor.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Diagnostics;
+using WMPLib;
+using System.IO;
+
+namespace VideoEditor
+{
+    public partial class FormEditor : Form
+    {
+        int heightColumn = 50;
+        int nrLines = 8;
+        Graphics editGraphic;
+        int mouseDown = 0;
+        int mouseDownCursor = 0;
+        public FormEditor()
+        {
+            InitializeComponent();
+            pnVideoEditing.AutoScroll = false;
+            pnVideoEditing.HorizontalScroll.Enabled = false;
+            pnVideoEditing.HorizontalScroll.Visible = false;
+            pnVideoEditing.HorizontalScroll.Maximum = 0;
+            pnVideoEditing.AutoScroll = true;
+            pnVideoEditing.AutoSize = true;
+            pnVideoEditing.Update();
+            var fullHeight = nrLines * heightColumn;
+            pnVideoEditing.AutoScrollMinSize = new Size(0, fullHeight);
+            pnVideoEditing.AutoScrollPosition = new Point(0, 0);
+            axWindowsMediaPlayer1.PlayStateChange += AxWindowsMediaPlayer1_PlayStateChange;
+        }
+
+        private void AxWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (e.newState == 9 || e.newState == 3)
+            {
+                timer1.Start();
+            }
+            else
+            {
+                if (e.newState == 2)
+                {
+                    timer1.Stop();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        void DrawVideoEditingLines()
+        {
+            editGraphic = pnVideoEditing.CreateGraphics();
+            Brush lineColor = Brushes.Black;
+            for (int i = 0; i < nrLines; i++)
+            {
+                editGraphic.DrawLine(new Pen(lineColor), new Point(0, i * heightColumn), new Point(pnVideoEditing.Width, i * heightColumn));
+            }
+            // Vertical Lines
+            editGraphic.DrawLine(new Pen(lineColor), new Point(heightColumn, 0), new Point(heightColumn, pnVideoEditing.Height));
+            pnVideoEditing.Location = new Point(0, this.Height - pnVideoEditing.Height);
+        }
+        private void pnVideoEditing_Paint(object sender, PaintEventArgs e)
+        {
+            DrawVideoEditingLines();
+        }
+
+        private void pnVideoEditing_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            Debug.WriteLine("Clicked: " + me.Location);
+        }
+
+        private void pnVideoEditing_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            string[] formats = e.Data.GetFormats();
+            if (files?.Count() > 0)
+            {
+                double duration = 0;
+                foreach (var file in files)
+                {
+                    duration += Duration(file);
+
+                    string extension = Path.GetExtension(file);
+                    if (extension == ".mp4")
+                    {
+
+                        Button item = new Button();
+                        item.Name = file + "," + duration;
+                        item.Click += Item_Click;
+                        item.Size = new Size((int)duration, 40);
+                        pnVideoEditing.Controls.Add(item);
+                        item.Location = new Point(heightColumn + 5, 5);
+                        pbCursor.Location = item.Location;
+                        pnVideoEditing.Update();
+                    }
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("No files added!");
+            }
+
+
+        }
+
+        private void Item_Click(object sender, EventArgs e)
+        {
+            Button crtButton = sender as Button;
+            if (axWindowsMediaPlayer1.URL != crtButton.Name.Split(',')[0] && !isPlaying())
+            {
+                axWindowsMediaPlayer1.URL = crtButton.Name.Split(',')[0];
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+
+        }
+        private bool isPlaying()
+        {
+            return axWindowsMediaPlayer1.playState == WMPPlayState.wmppsReady || axWindowsMediaPlayer1.playState == WMPPlayState.wmppsPlaying;
+        }
+        private void pnVideoEditing_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void pnVideoEditing_DragLeave(object sender, EventArgs e)
+        {
+        }
+        public double Duration(string file)
+        {
+            IWMPMedia mediainfo = axWindowsMediaPlayer1.newMedia(file);
+            return mediainfo.duration;
+        }
+
+        private void FormEditor_ClientSizeChanged(object sender, EventArgs e)
+        {
+            DrawVideoEditingLines();
+        }
+
+        private void pnVideoEditing_Enter(object sender, EventArgs e)
+        {
+        }
+
+        private void pnVideoEditing_MouseEnter(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Mouse enter: " + sender.GetType());
+            Cursor.Current = Cursors.Cross;
+        }
+
+        private void pnVideoEditing_MouseHover(object sender, EventArgs e)
+        {
+            if (mouseDown == 1)
+            {
+                pnVideoEditing.Location = new Point(pnVideoEditing.Location.X, this.PointToClient(Cursor.Position).Y);
+            }
+        }
+
+        private void pnVideoEditing_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Location.Y < 3)
+            {
+                if (Cursor.Current != Cursors.SizeNS)
+                {
+                    Cursor.Current = Cursors.SizeNS;
+                }
+                if (mouseDown == 1)
+                {
+                    pnVideoEditing.Location = new Point(pnVideoEditing.Location.X, this.PointToClient(Cursor.Position).Y);
+                }
+            }
+            else
+            {
+                if (Cursor.Current != Cursors.Default)
+                {
+                    Cursor.Current = Cursors.Default;
+                }
+            }
+            Debug.WriteLine("Mouse move: " + e.Location);
+        }
+
+        private void pnVideoEditing_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (mouseDown != 1)
+            {
+                mouseDown = 1;
+            }
+        }
+
+        private void pnVideoEditing_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (mouseDown != 0)
+            {
+                mouseDown = 0;
+            }
+        }
+
+        private void FormEditor_KeyDown(object sender, KeyEventArgs e)
+        {
+            Debug.WriteLine("KeyDown: " + e.KeyCode);
+            if (isPlaying())
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.pause();
+            }
+            else
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+        }
+
+        private void FormEditor_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Debug.WriteLine("KeyPress: " + e.KeyChar);
+        }
+
+        private void FormEditor_SizeChanged(object sender, EventArgs e)
+        {
+            DrawVideoEditingLines();
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pbCursor.Location = new Point(heightColumn + (int)axWindowsMediaPlayer1.Ctlcontrols.currentPosition, pbCursor.Location.Y);
+        }
+
+        private void pbCursor_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbCursor_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDownCursor == 1)
+            {
+                pbCursor.Location = new Point(e.Location.X, pbCursor.Location.Y);
+            }
+        }
+
+        private void pbCursor_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (mouseDownCursor != 1)
+            {
+                mouseDownCursor = 1;
+            }
+        }
+
+        private void pbCursor_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (mouseDownCursor != 0)
+            {
+                mouseDownCursor = 0;
+            }
+        }
+
+        private void pbCursor_MouseHover(object sender, EventArgs e)
+        {
+            if (mouseDownCursor == 1)
+            {
+                pbCursor.Location = new Point(pnVideoEditing.PointToClient(Cursor.Position).X, pbCursor.Location.Y);
+            }
+        }
+    }
+}
